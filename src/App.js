76f6467201cc00b825908b900c2ec57a3ba8ba0e
rosudoria <77439184+rosudoria@users.noreply.github.com>
2021-12-20 18:13:42 -0800

    Update App.js
    Import pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
